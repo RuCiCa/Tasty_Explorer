45bd7220ae14137218b019b51212e3f03f9e4b4b
WAFFLE900 <thomasliounk@gmail.com>
2023-06-13 10:18:57 +0800

    1

--- frontend/frontend/src/components/lists.vue
+++ frontend/frontend/src/components/lists.vue
@@ -88,6 +88,31 @@ export default {
     };
   },
   mounted() {
+    const path = "http://localhost:5000/list";
+    const user_id = this.user_id;
+
+    axios
+      // post 過去的東西要包大括號 {}
+      .post(path, {user_id})
+      .then((res) => {
+        // console.log(test_time);
+        this.comment_count = res.data.comment_count;
+        this.diary_count = res.data.diary_count;
+        this.list_count = res.data.list_count;
+        this.follower_count = res.data.follower_count;
+        this.following_count = res.data.following_count;
+        this.user_name = res.data.info[0].user_name;
+        for (var i = 0; i < res.data.diary.length; i++) {
+          this.diarys.push({ id: res.data.diary[i].diary_id, time: res.data.diary[i].date_visited, rest_name: res.data.diary[i].restaurant_name, diary_text: res.data.diary[i].diary_content });
+        }
+        // console.log(res.data.diary);
+
+
+
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
   },
   beforeUnmount() {
